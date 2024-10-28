c644abbbc2b4ffd70474c2b9bd785a099af9896d
rooseveltalej <roperez@estudiantec.cr>
2024-10-28 15:00:19 -0600

    feat: add input field for dynamic veterinarian ID in Histories component

--- src/pages/Histories.js
+++ src/pages/Histories.js
@@ -1,28 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import HistoryList from '../components/HistoryList';
 
 const Histories = () => {
   const [historiales, setHistoriales] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [idVeterinario, setIdVeterinario] = useState('');  // Estado para el ID ingresado por el usuario
 
   const fetchHistoriales = async () => {
-    const response = await fetch('http://127.0.0.1:8000/check-table/?tabla=Historial');
-    const data = await response.json();
-    if (response.ok) {
-      setHistoriales(data.data);
-    } else {
-      console.error('Error fetching historiales:', data.message);
+    setLoading(true);
+    try {
+
+      if (!idVeterinario) {
+        const response = await fetch('http://127.0.0.1:8000/check-table/?tabla=Historial');
+        const data = await response.json();
+        if (response.ok) {
+          setHistoriales(data.data);
+        } else {
+          console.error('Error fetching historiales:', data.message);
+        }
+      }
+
+      else {
+
+
+      const response = await fetch(`http://127.0.0.1:8000/citas-veterinario/${idVeterinario}`, {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        setHistoriales(data.data);
+      } else {
+        console.error('Error fetching historiales:', data.message);
+      } }
+    
+    
+    
+    
+    } catch (error) {
+      console.error('Error fetching historiales:', error);
     }
+    setLoading(false);
   };
 
-  useEffect(() => {
-    fetchHistoriales();
-    setLoading(false);
-  }, []);
-
   return (
-    <div className="container mx-auto">
-      {loading ? <p>Cargando historiales...</p> : <HistoryList historiales={historiales} />}
+    <div className="container mt-auto mx-auto p-4">
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Ingrese el ID del veterinario"
+          value={idVeterinario}
+          onChange={(e) => setIdVeterinario(e.target.value)}
+          className="border p-2 rounded mr-2"
+        />
+        <button onClick={fetchHistoriales} className="bg-blue-500 text-white p-2 rounded">
+          Buscar Historiales
+        </button>
+      </div>
+      
+      {loading ? (
+        <p>Cargando historiales...</p>
+      ) : (
+        <HistoryList historiales={historiales} />
+      )}
     </div>
   );
 };
