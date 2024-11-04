539c3e033144f5f94957fd4b7d2fd02793104d98
FabsCR <fabfernandez@estudiantec.cr>
2024-11-04 01:37:51 -0600

    feat(dashboard): add responsive UI with modals, date filters, and PDF generation button

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import Login from './pages/Login';
 import Clients from './pages/Clients';
 import Appointments from './pages/Appointments';
 import Histories from './pages/Histories';
-import Reports from './pages/Reports'; // Importa la página de reportes
+import Reports from './pages/Reports';
 import UserProfile from './pages/UserProfile';
+import Dashboard from './pages/Dashboard';
 
 // Función para decodificar el token JWT y obtener el payload
 const decodeToken = (token) => {
   try {
-    const payloadBase64 = token.split('.')[1]; // El payload es la segunda parte del token
-    const decodedPayload = JSON.parse(atob(payloadBase64)); // Decodifica de Base64 y convierte a JSON
+    const payloadBase64 = token.split('.')[1];
+    const decodedPayload = JSON.parse(atob(payloadBase64));
     return decodedPayload;
   } catch (error) {
     console.error("Error al decodificar el token:", error);
@@ -20,27 +21,24 @@ const decodeToken = (token) => {
   }
 };
 
-// Componente que protege las rutas privadas y verifica roles
+// Componente para proteger las rutas y verificar el rol
 const PrivateRoute = ({ children, allowedRoles }) => {
-  const token = localStorage.getItem('token'); // Obtén el token del localStorage
+  const token = localStorage.getItem('token');
   let userRole = null;
 
-  // Decodifica el token para extraer el rol
   if (token) {
     const decodedToken = decodeToken(token);
     if (decodedToken) {
-      userRole = decodedToken.role; // Asegúrate de que el token contenga un campo 'role'
+      userRole = decodedToken.role;
     } else {
       return <Navigate to="/login" />;
     }
   }
 
-  // Verifica si el token existe y si el rol está permitido
   if (token && allowedRoles.includes(userRole)) {
     return children;
   }
 
-  // Si no hay token o el rol no es permitido, redirige a login
   return <Navigate to="/login" />;
 };
 
@@ -57,14 +55,15 @@ function App() {
   return (
     <Router>
       <div className="bg-gray-100 min-h-screen">
-        {token && <Navbar />}  
+        {token && <Navbar />}
         <Routes>
           <Route path="/login" element={<Login setToken={setToken} />} />
           <Route path="/" element={<PrivateRoute allowedRoles={['Administrador', 'Recepcionista', 'Veterinario']}><Clients /></PrivateRoute>} />
           <Route path="/citas" element={<PrivateRoute allowedRoles={['Administrador', 'Recepcionista', 'Veterinario']}><Appointments /></PrivateRoute>} />
           <Route path="/historiales" element={<PrivateRoute allowedRoles={['Administrador', 'Recepcionista', 'Veterinario']}><Histories /></PrivateRoute>} />
-          <Route path="/reportes" element={<PrivateRoute allowedRoles={['Administrador', 'Recepcionista']}><Reports /></PrivateRoute>} /> {/* Ruta protegida */}
+          <Route path="/reportes" element={<PrivateRoute allowedRoles={['Administrador', 'Recepcionista']}><Reports /></PrivateRoute>} />
           <Route path='/user-profile' element={<PrivateRoute allowedRoles={['Administrador', 'Recepcionista', 'Veterinario']}><UserProfile /></PrivateRoute>} />
+          <Route path="/dashboard" element={<PrivateRoute allowedRoles={['Administrador']}><Dashboard /></PrivateRoute>} />
         </Routes>
       </div>
     </Router>
